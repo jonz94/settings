8fdb818bf11ecdcc239f1a0067ecb1b3517f5792
jonz94 <jody16888@gmail.com>
2022-03-29 09:37:10 +0800

    ✨ feat: tweak home page's text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
       <div className="hero-content text-center">
         <div className="max-w-md">
           <h1 className="mb-5 text-5xl font-bold">Hello, world!</h1>
-          <p className="mb-5">Hope you enjoy all the settings shared by me!</p>
+          <p className="mb-5">Hope you will enjoy the settings shared by me!</p>
         </div>
       </div>
     </>
