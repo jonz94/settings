3c4c7039f831cc9d415db54d92ef654bca01563e
jonz94 <jody16888@gmail.com>
2022-06-14 06:03:09 +0800

    👍 build(dev-deps): remove unnecessary `@types/tailwindcss` package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
-/** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
