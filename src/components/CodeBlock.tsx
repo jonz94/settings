736400c18dbcb1c439e3a48252b0703037612b7f
jonz94 <jody16888@gmail.com>
2022-06-18 22:36:01 +0800

    ✅ fix: resolve layout issue when <CodeBlock> is still loading data

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -9,13 +9,16 @@ const CodeBlock: FC<Props> = (props) => {
 
   return (
     <>
+      {/* loading */}
+      <div className={`${isLoading ? '' : 'hidden'} mx-auto`}>
+        <pre className="shiki"></pre>
+      </div>
+
+      {/* data loaded */}
       <div
         className={`${isLoading ? 'hidden' : ''} text-md mx-auto whitespace-pre-wrap sm:text-lg md:text-xl`}
         dangerouslySetInnerHTML={{ __html: props.settingsJson }}
       ></div>
-      <div className={isLoading ? '' : 'hidden'}>
-        <pre className="shiki"></pre>
-      </div>
     </>
   )
 }
