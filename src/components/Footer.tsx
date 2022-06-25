20d5809861153f09ebb31470778d7bdd3631c61c
jonz94 <jody16888@gmail.com>
2022-06-25 19:02:01 +0800

    ✅ fix: use image for emoji

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,17 @@
+import Image from 'next/image'
 import { FC } from 'react'
 
 const Footer: FC = () => {
   return (
     <footer className="footer footer-center bg-base-300 p-4 text-base-content">
       <div>
-        <p>Built with ❤️ by jonz94</p>
+        <p className="space-x-[1ch]">
+          <span>Built with</span>
+          <span className="relative top-1">
+            <Image src="/noto-emoji-u2764.png" height={16} width={16} alt="♥" priority />
+          </span>
+          <span>by jonz94</span>
+        </p>
       </div>
     </footer>
   )
