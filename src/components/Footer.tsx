1710c9292d0ca095b36613e9da7e96361d3f0255
jonz94 <jody16888@gmail.com>
2022-11-24 07:23:45 +0800

    ✅ fix: correct footer layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: FC = () => {
   return (
     <footer className="footer footer-center bg-base-300 p-4 text-base-content">
       <div>
-        <p className="space-x-[1ch]">
+        <p className="flex space-x-[1ch]">
           <span>Built with</span>
           <span className="relative top-1">
             <Image src="/noto-emoji-u2764.png" height={16} width={16} alt="♥" priority />
